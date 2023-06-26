374d7cc169b4c61384e4e61307c4e49929b39a5c
gonzabeade <gonzabeade@gmail.com>
2023-06-26 15:06:43 -0300

    Initialize project

---
+++ src/main/java/ar/edu/itba/bd2/redmond/ItbaBd2Application.java
@@ -0,0 +1,13 @@
+package ar.edu.itba.bd2.redmond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItbaBd2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItbaBd2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/itba/bd2/redmond/ItbaBd2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.itba.bd2.redmond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItbaBd2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
