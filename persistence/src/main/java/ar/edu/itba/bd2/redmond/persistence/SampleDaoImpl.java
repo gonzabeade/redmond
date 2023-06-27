0211227955c403e6057e78d53277b92317fca7ab
gonzabeade <gonzabeade@gmail.com>
2023-06-27 16:36:36 -0300

    Implement connection to postgres

--- persistence/src/main/java/ar/edu/itba/bd2/redmond/persistence/SampleDaoImpl.java
+++ persistence/src/main/java/ar/edu/itba/bd2/redmond/persistence/SampleDaoImpl.java
@@ -1,14 +1,27 @@
 package ar.edu.itba.bd2.redmond.persistence;
 
 import ar.edu.itba.bd2.redmond.model.SampleModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class SampleDaoImpl implements SampleDao {
 
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public SampleDaoImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public SampleModel newSampleDao(String name) {
-        return new SampleModel(name);
 
+        // Example database operation using JDBC template
+        String sql = "INSERT INTO sample (column1, column2) VALUES (?, ?)";
+        jdbcTemplate.update(sql, "value1", "value2");
+
+        return new SampleModel(name);
     }
 }
