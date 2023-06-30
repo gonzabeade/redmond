6e6c32cfbb4f6dec7e2fbbf1a4c8ac1a67fca727
Agustín Morantes <agustinmorantes@gmail.com>
2023-06-29 22:56:02 -0300

    Add OpenApiDefinition to Bank API

--- bank/src/main/kotlin/com/redmond/bankapi/RedmondBankApiApplication.kt
+++ bank/src/main/kotlin/com/redmond/bankapi/RedmondBankApiApplication.kt
@@ -1,10 +1,20 @@
 package com.redmond.bankapi
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.servers.Server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.transaction.annotation.EnableTransactionManagement
 
+@OpenAPIDefinition(
+    servers = [
+        Server(
+            url = "/",
+            description = "Local server"
+        )
+    ]
+)
 @EnableJpaRepositories("com.redmond.bankapi.repositories.jpa")
 @EnableTransactionManagement
 @SpringBootApplication
